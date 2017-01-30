0fcade55ed6f7ca81ddcf27a08f31929d5807552
Alvaro Flores <alvaro.flores01@gmail.com>
2017-01-30 12:30:49 -0800

    adds calculator function

--- checkout.php
+++ checkout.php
@@ -1,7 +1,17 @@
-<?php $firstName = $_GET["firstName"]; ?>
-<?php $lasttName = $_GET["lastName"]; ?>
-<?php $address = $_GET["address"]; ?>
+<?php
+    $firstName = $_GET["firstName"];
+    $lasttName = $_GET["lastName"];
+    $address = $_GET["address"];
+    $input_weight = $_GET["weight"];
+    $input_distance = $_GET["distance"];
+    $shipping_price = calculator($input_weight, $input_distance);
 
+    function calculator($weight, $distance)
+    {
+      $price = ($distance/20 + $weight/20);
+      return $price;
+    }
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -10,7 +20,7 @@
   </head>
   <body>
     <h2>Checkout</h2>
-    <p>Your Total will be 6.99</p>
+    <p>Your Total will be <?php echo $shipping_price;?>$</p>
     <p>Thank you for shopping on my online store <?php echo "$firstName"; ?> <?php echo "$lasttName"; ?></p>
     <p>Your purchase of socks will be dilevered at <?php echo "$address";?>. Come again!!</p>
   </body>
